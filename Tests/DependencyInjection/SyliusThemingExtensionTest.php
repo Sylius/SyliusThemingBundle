2c01133f204572092f07e0ecb423dd826a52be88
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2011-11-16 22:36:10 +0100

    added test and .gitignore.

---
+++ Tests/DependencyInjection/SyliusThemingExtensionTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\ThemingBundle\Tests\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Sylius\Bundle\ThemingBundle\DependencyInjection\SyliusThemingExtension;
+use Symfony\Component\Yaml\Parser;
+
+class SyliusThemingExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    protected $configuration;
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testUserLoadThrowsExceptionUnlessDriverSet()
+    {
+        $loader = new SyliusThemingExtension();
+        $config = $this->getEmptyConfig();
+        unset($config['driver']);
+        $loader->load(array($config), new ContainerBuilder());
+    }
+
+    /**
+     * getEmptyConfig
+     *
+     * @return array
+     */
+    protected function getEmptyConfig()
+    {
+        $yaml = <<<EOF
+driver: ORM
+classes:
+    model:
+        theme: Sylius\Bundle\ThemingBundle\Entity\Theme
+EOF;
+        $parser = new Parser();
+
+        return $parser->parse($yaml);
+    }
+
+    protected function tearDown()
+    {
+        unset($this->configuration);
+    }
+}
